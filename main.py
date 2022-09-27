36af8d606fb11cd62523abfdc1d6e615f53bcee0
Niño James Tan <njtan142@gmail.com>
2022-09-27 19:46:33 +0800

    minor update

--- main.py
+++ main.py
@@ -1,34 +1,84 @@
 import pygame
 import sys
+import time
 from Letter import Letter
 from Scene1 import SceneOne, MainMenuScene, LevelSelection
 
-print("Welcome to Word Finder")
-username = input("Press enter to play game:")
-print("Game modes:")
-print("Easy - 1")
-print("Medium - 2")
-print("Hard - 3")
+print("""
+
+            ░██╗░░░░░░░██╗░█████╗░██████╗░██████╗░  ███████╗██╗███╗░░██╗██████╗░███████╗██████╗░
+            ░██║░░██╗░░██║██╔══██╗██╔══██╗██╔══██╗  ██╔════╝██║████╗░██║██╔══██╗██╔════╝██╔══██╗
+            ░╚██╗████╗██╔╝██║░░██║██████╔╝██║░░██║  █████╗░░██║██╔██╗██║██║░░██║█████╗░░██████╔╝
+            ░░████╔═████║░██║░░██║██╔══██╗██║░░██║  ██╔══╝░░██║██║╚████║██║░░██║██╔══╝░░██╔══██╗
+            ░░╚██╔╝░╚██╔╝░╚█████╔╝██║░░██║██████╔╝  ██║░░░░░██║██║░╚███║██████╔╝███████╗██║░░██║
+            ░░░╚═╝░░░╚═╝░░░╚════╝░╚═╝░░╚═╝╚═════╝░  ╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═════╝░╚══════╝╚═╝░░╚═╝  """)
+
+print("\n\n")
+user = input(
+    "                                          PRESS ENTER TO START                                                                ")
+
+loading = "░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒██████████████████████████████████████████████████████"
+for char in loading:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+
+    time.sleep(0.02)
+
+print("\n\n")
+print("""                                               [1] EASY                                    """)
+print("""                                               [2] HARD                                      """)
+
 category = input("Enter your game mode:")
 match int(category):
     case 1:
         print('You selected easy mode')
     case 2:
-        print('You selected medium mode')
-    case 3:
         print('You selected hard mode')
     case other:
         print('Wrong input, please restart the game')
         sys.exit()
+easyLevel1 = ['plain', 'river', 'herb', 'arctic', 'ivy', 'valley']
+easyLevel2 = ['yam', 'lobster','sparrow','parrot','bear','tick']
+easyLevel3 = ['neongreen','neonblue','peach','purple','yellowgreen']
 
-level = input("Select level (1 to 5)")
+hardLevel1 = ['sand','raspberry','lilac','cyan','sapphire','amaranth','ochre','amber','slategray','apricot']
+hardLevel2 = ['vienna','honolulu','liecester','riverside','geneva','leipzig','seattle','prague','kabul','budapest']
+hardLevel3 = ['bamboo','season','coast','waterfall','savanna','rainforest','ocean','marsh','reed']
+hardLevel4 = ['toaster','duvet','curtains','cupboard','bath','mattress','espresso','blanket','wardrobe']
+hardLevel5 = ['weasel','vicuna','opossum','crane','swan','chicken','dolphin','guanaco','lemming']
+
+words = []
+if category == 1:
+    level = input("Select level (1 to 3)")
+    match int(level):
+        case 1:
+            words = easyLevel1
+        case 2:
+            words = easyLevel2
+        case 3:
+            words = easyLevel3
+        case other:
+            sys.exit()
+else:
+    level = input("Select lvel (1 to 5)")
+    match int(level):
+        case 1:
+            words = hardLevel1
+        case 2:
+            words = hardLevel2
+        case 3:
+            words = hardLevel3
+        case 4:
+            words = hardLevel4
+        case 5:
+            words = hardLevel5
+
 
 print("Your game is loaded in a new window")
 
 size = width, height = 1000, 700
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Word Search')
-
 
 white = (255, 255, 255)
 black = (0, 0, 0)
@@ -49,7 +99,6 @@ match int(category):
         s1.levelSelected("Hard | " + "Level " + level)
 
 
-
 class Player:
     def __init__(self):
         self.x = 100
@@ -57,6 +106,7 @@ class Player:
 
 
 player = Player()
+
 
 def redraw():
     screen.fill(black)
@@ -77,10 +127,7 @@ def handlePress():
     elif pressed[pygame.K_d]:
         player.x += 31
     elif pressed[pygame.K_ESCAPE]:
-            sys.exit()
-
-
-
+        sys.exit()
 
 
 while 1:
@@ -96,4 +143,3 @@ while 1:
     s1.generate()
     # pygame.draw.rect(screen, (50, 50, 50), (player.x, player.y, 31, 31), 3)
     pygame.display.update()
-
